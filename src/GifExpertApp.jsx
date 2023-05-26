88ecfa371aa18041e1c43b427e1c67fa75d069e6
Camilo Puentes <jcpsandoval94@gmail.com>
2023-05-26 11:31:10 -0500

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+      {/* Titulo */}
+      <h1>GiftExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de git */}
+      {/* Gif item */}
+    </>
+  )
+}
